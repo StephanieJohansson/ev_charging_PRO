532ad5ede538403f6f19b5b9d01ea95839323bb3
Stephanie Johansson <3tomu24.johast@folkuniversitetet.nu>
2025-12-03 10:59:40 +0100

    first push

---
+++ src/main/java/com/stephanie/ev_charging_pro/EvChargingProApplication.java
@@ -0,0 +1,13 @@
+package com.stephanie.ev_charging_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvChargingProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvChargingProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stephanie/ev_charging_pro/EvChargingProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stephanie.ev_charging_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvChargingProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
