9a6c653aa0846cbed9a1602d95b86a5a2fc3e4d5
Stephanie Johansson <3tomu24.johast@folkuniversitetet.nu>
2026-03-24 11:51:08 +0100

    creating test application-test.properties to configure H2

--- src/test/java/com/stephanie/ev_charging_pro/EvChargingProApplicationTests.java
+++ src/test/java/com/stephanie/ev_charging_pro/EvChargingProApplicationTests.java
@@ -2,9 +2,12 @@ package com.stephanie.ev_charging_pro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 // integration test to test the whole application
+// with h2 test database
 @SpringBootTest
+@ActiveProfiles("test")
 class EvChargingProApplicationTests {
 
 	@Test
